2be0747754b285afdbdce07a06ee38099bbd9dc9
YASHSAWANTTT <yasawant@asu.edu>
2024-06-12 21:45:03 +0400

    initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+// https://active-lamb-59.clerk.accounts.dev
+const authConfig = {
+    providers: [
+      {
+        domain: "https://active-lamb-59.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
